79f1b33918d32aff3d4537470d95eaff9663a61d
Pranav <102531883+its-pranav@users.noreply.github.com>
2023-01-30 14:44:02 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Connect to the MongoDB database
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const db = client.db("test");
+  console.log("Connected to MongoDB!");
+});
